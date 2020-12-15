8dd8c02a0bfa65d19234421bcd698cc564eb6359
hajinsu <hajinsu@lotte.net>
2020-12-15 13:10:09 +0900

    Initial commit

---
+++ app/src/main/java/com/handroid/newbonin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.handroid.newbonin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.main_view_button)
+
+        btn.setOnClickListener {
+
+        }
+
+    }
+}
